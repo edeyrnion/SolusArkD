fc4e6a9033d4ab0624beb4db26d9a6bf1c1b981b
edeyrnion <matthias.treubrodt@gmail.com>
2018-11-16 13:14:39 +0100

    Thread Safe Generic Singleton

---
+++ Assets/MattsStuff/Scripts/Utils/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Matthias
+{
+    // Generic Singleton (Thread Safe)
+    public class Singleton<T> : MonoBehaviour where T : class, new()
+    {
+        protected Singleton() { }
+
+        public static T Instance
+        {
+            get
+            {
+                return SingletonCreator.instance;
+            }
+        }
+
+        private class SingletonCreator
+        {
+            static SingletonCreator() { }
+
+            internal static readonly T instance = new T();
+        }
+    }
+}
