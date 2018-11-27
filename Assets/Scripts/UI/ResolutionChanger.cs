add61bdf58efd283a6822243a37b87710b4ffacc
DerThaar <wazap@gmx.de>
2018-11-27 18:12:21 +0100

    Started adding ResolutionChanger

--- Assets/Scripts/UI/ResolutionChanger.cs
+++ Assets/Scripts/UI/ResolutionChanger.cs
@@ -6,14 +6,38 @@ using TMPro;
 public class ResolutionChanger : MonoBehaviour
 {
 	[SerializeField] TMP_Dropdown resolutionDropDown;
+	List<TMP_Dropdown.OptionData> optionDatas = new List<TMP_Dropdown.OptionData>();
+	Resolution[] resolutions;
+	bool fullscreen;
 
 
 	void Start()
 	{
-
+		resolutionDropDown.ClearOptions();
+		resolutions = Screen.resolutions;
+		for (int i = 0; i < resolutions.Length; i++)
+		{
+			int width = resolutions[i].width;
+			int height = resolutions[i].height;
+			TMP_Dropdown.OptionData optionData = new TMP_Dropdown.OptionData();
+			optionData.text = $"{width}x{height}";
+			optionDatas.Add(optionData);
+		}
+		resolutionDropDown.AddOptions(optionDatas);
+		int currentWidth = Screen.currentResolution.width;
+		int currentHeight = Screen.currentResolution.height;
+		string currentRes = $"{currentWidth}x{currentHeight}";
+		for (int i = 0; i < resolutionDropDown.options.Count; i++)
+		{
+			if (resolutionDropDown.options[i].text == currentRes)
+			{
+				int value = i;
+				resolutionDropDown.value = value;
+				return;
+			}
+		}
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
 
--- Assets/Scripts/Utils/ExtensionMethodes.cs
+++ Assets/Scripts/Utils/ExtensionMethodes.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public static class ExtensionMethodes
 {
 	public static Vector3 ToVector3(this Vector2 v2)
 	{
 		return new Vector3(v2.x, 0, v2.y);
-	}	
+	}
+
+	public static List<Resolution> ToList(this Resolution[] array)
+	{
+		return new List<Resolution>(array);
+	}
 }
