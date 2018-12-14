ebd28c58ee842a5378ab0271a4d2934b5a77da31
edeyrnion <matthias.treubrodt@gmail.com>
2018-12-14 10:29:51 +0100

    Bones Controller.

---
+++ Assets/Prefabs/Bones/Script/BonesController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+public class BonesController : MonoBehaviour
+{
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Move(float speed)
+    {
+        anim.SetFloat("Speed", speed);
+    }
+}
