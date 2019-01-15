ce533db633ef5c5c83e6c02323afb296b9196c10
Der_Thaar <wazap@gmx.de>
2019-01-15 12:29:09 +0100

    Fixed some minor stuff

---
+++ Assets/Resources/README.txt
@@ -0,0 +1,10 @@
+Controlls:
+
+Attack:		Mouse 1	/	Right Bumper
+Jump:		Space	/	B (Circle)
+Lantern:		F		/	X (Square)
+Crouch:		C		/	Left Trigger
+Walk:			Shift		/	Left Bumper
+Pause:		Esc		/	Start
+
+The lantern is needed to kill the boss.
---
+++ Assets/Scripts/Utils/HandleTextFile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.IO;
+using TMPro;
+
+public class HandleTextFile : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI text;
+
+	void Start()
+	{
+		string path = "Assets/Resources/README.txt";
+
+		StreamReader reader = new StreamReader(path);
+		text.fontSize = 30;		
+		text.text = reader.ReadToEnd();
+		reader.Close();
+	}
+}
